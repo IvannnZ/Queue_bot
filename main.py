26bffa53d3a1c376bf857d28be3914a4facfd8f3
IvannZ <zhukov.ivan05@yandex.ru>
2024-11-05 11:27:12 +0700

    v1.0 fulli make, user_test_start

--- main.py
+++ main.py
@@ -31,11 +31,11 @@ markup_FULL_User = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
 markup_FULL_User.row('Создать очередь', 'Встать в очередь', 'посмотреть где я')
 
 
-def send_User_or_Admin(id, text):
-    if id in Admins:
-        msg = bot.send_message(id, text, reply_markup=markup_FULL_Admin)
+def send_User_or_Admin(id_User_or_Admin, text):
+    if id_User_or_Admin in Admins:
+        msg = bot.send_message(id_User_or_Admin, text, reply_markup=markup_FULL_Admin)
     else:
-        msg = bot.send_message(id, text, reply_markup=markup_FULL_User)
+        msg = bot.send_message(id_User_or_Admin, text, reply_markup=markup_FULL_User)
     return msg
 
 
@@ -47,7 +47,7 @@ def start_handler(message):
 @bot.message_handler(func=lambda message: message.text == 'Создать очередь')
 def create_queue(message):
     for i in Admins:
-        if i.id == message.chat.id:
+        if Admins[i].id_Admin == message.chat.id:
             bot.send_message(message.chat.id, "Вы уже админ очереди, вы можете быть админом только 1 очереди",
                              reply_markup=markup_FULL_Admin)
     msg = bot.send_message(message.chat.id, "Введи своё имя", reply_markup=markup_empty)
@@ -55,10 +55,10 @@ def create_queue(message):
 
 
 def create_queue1(message):
-    bot.send_message(message.chat.id, "Отлично, очередь создана\nP.s. ты в ней не стоишь, встань в неё сам",
-                     reply_markup=markup_FULL_Admin)
     Admins[message.chat.id] = Admin(message.chat.id, message.text)
     Users[message.chat.id] = User(message.chat.id, message.text)
+    bot.send_message(message.chat.id, "Отлично, очередь создана\nP.s. ты в ней не стоишь, встань в неё сам",
+                     reply_markup=markup_FULL_Admin)
 
 
 @bot.message_handler(func=lambda message: message.text == 'Встать в очередь')
@@ -68,7 +68,10 @@ def join_queue_request(message):
                          reply_markup=markup_FULL_User)
     else:
         if message.chat.id in Users:
-            queue_list = "\n".join([f"#{admin_num} :{Admins[admin_num].name}" for admin_num in range(len(Admins))])
+            queue_list_l = list()
+            for i in Admins:
+                queue_list_l.append(Admins[i].name)
+            queue_list = "\n".join([f"#{i} :{queue_list_l[i]}" for i in range(len(queue_list_l))])
             msg = bot.send_message(message.chat.id, f"Выберите очередь, отправив её номер:\n{queue_list}",
                                    reply_markup=markup_empty)
             bot.register_next_step_handler(msg, join_queue)
@@ -79,7 +82,11 @@ def join_queue_request(message):
 
 def create_user(message):
     Users[message.chat.id] = User(message.chat.id, message.text)
-    queue_list = "\n".join([f"#{admin_num} :{Admins[admin_num].name}" for admin_num in range(len(Admins))])
+    queue_list_l = list()
+    for i in Admins:
+        queue_list_l.append(Admins[i].name)
+    queue_list = "\n".join([f"#{i} :{queue_list_l[i]}" for i in range(len(queue_list_l))])
+
     msg = bot.send_message(message.chat.id, f"Выберите очередь, отправив её номер:\n{queue_list}",
                            reply_markup=markup_empty)
     bot.register_next_step_handler(msg, join_queue)
@@ -89,8 +96,14 @@ def join_queue(message):
     try:
         queue_id = int(message.text)
         if queue_id < len(Admins):
-            if message.from_user.id not in Admins[queue_id]:
-                Admins[queue_id].add_user(Users[message.chat.id])
+            admin_num = 0
+            for i in Admins:
+                if queue_id == 0:
+                    admin_num = i
+                    break
+                queue_id -= 1
+            if message.from_user.id not in Admins[admin_num]:
+                Admins[admin_num].add_user(Users[message.chat.id])
 
                 send_User_or_Admin(message.chat.id, f"Ты добавлен в очередь #{queue_id}. Ожидай своей очереди.")
             else:
@@ -98,20 +111,23 @@ def join_queue(message):
         else:
             send_User_or_Admin(message.chat.id, "Очередь с таким номером не найдена.")
     except ValueError:
-        queue_list = "\n".join([f"#{admin_num} :{Admins[admin_num].name}" for admin_num in range(len(Admins))])
+        queue_list_l = list()
+        for i in Admins:
+            queue_list_l.append(Admins[i].name)
+        queue_list = "\n".join([f"#{i} :{queue_list_l[i]}" for i in range(len(queue_list_l))])
+
         msg = bot.send_message(message.chat.id, f"Пожалуйста, отправь номер очереди.\n{queue_list}",
                                reply_markup=markup_empty)
         bot.register_next_step_handler(msg, join_queue)
 
 
-@bot.message_handler(commands=['посмотреть где я'])
+@bot.message_handler(func=lambda message: message.text == 'посмотреть где я')
 def my_position(message):
-    position = None
     answ_l = []
     for i in Admins:
-        pos = i.search_by_id(message.chat.id)
+        pos = Admins[i].admin_queue.search_by_id(message.chat.id)
         if pos is not None:
-            answ_l.append((i.name, pos))
+            answ_l.append((Admins[i].name, pos))
     if answ_l:
         answ = "\n".join(f"Ты находишься на {poss} месте в очереди {name}" for name, poss in answ_l)
         send_User_or_Admin(message.chat.id, answ)
@@ -119,23 +135,27 @@ def my_position(message):
         send_User_or_Admin(message.chat.id, "Ты не стоишь ни в одной из очередей.")
 
 
-@bot.message_handler(commands=['следующий'])
+@bot.message_handler(func=lambda message: message.text == 'следующий')
 def next_in_queue(message):
     if message.chat.id in Admins:
         now, next = Admins[message.chat.id].next_in_queue()
         if now is None:
             del Admins[message.chat.id]
-            bot.send_message("И так, поздравляю, больше нет людей в очереди", reply_markup=markup_FULL_User)
+            bot.send_message(message.chat.id, "И так, поздравляю, больше нет людей в очереди",
+                             reply_markup=markup_FULL_User)
             return
         elif next is None:
+            send_User_or_Admin(now.id_User,
+                               f"Ты сейчас сдаёшь, подойди для сдачи в очередь: {Admins[message.chat.id].name}")
             del Admins[message.chat.id]
-            bot.send_message(
-                "И так, поздравляю, это последний человек в очереди, но пользователи всё ещё могут добавиться",
-                reply_markup=markup_FULL_Admin)
+            bot.send_message(message.chat.id,
+                             "И так, поздравляю, это последний человек в очереди, но пользователи всё ещё могут добавиться",
+                             reply_markup=markup_FULL_User)
             return
         else:
-            send_User_or_Admin(now.id, f"Ты сейчас сдаёшь, подойди для сдачи в очередь: {Admins[message.chat.id].name}")
-            send_User_or_Admin(next.id,
+            send_User_or_Admin(now.id_User,
+                               f"Ты сейчас сдаёшь, подойди для сдачи в очередь: {Admins[message.chat.id].name}")
+            send_User_or_Admin(next.id_User,
                                f"Ты сейчас сдаёшь следующим, подойди для сдачи в очередь: {Admins[message.chat.id].name}")
             send_User_or_Admin(message.chat.id,
                                f"Сейчас сдаёт: {now.name}\nследующий: {next.name}\nвсего в очереди: {Admins[message.chat.id].lenght()}")
@@ -144,5 +164,6 @@ def next_in_queue(message):
         bot.send_message(message.chat.id,
                          "ЙООООу, так, либо ты сломал бота, либо у меня что-то не так, так или иначе напиши мне @Not_IvannZ")
         return
+
 
 bot.polling(none_stop=True)
--- queue_for_bot.py
+++ queue_for_bot.py
@@ -1,9 +1,9 @@
-from queue import Queue as Q
+from re import search
 
 
 class User:
-    def __init__(self, id, name="base_name"):
-        self.id = id
+    def __init__(self, id_User, name="base_name"):
+        self.id_User = id_User
         self.name = name
         self.queues = []
 
@@ -14,12 +14,12 @@ class User:
         self.queues.append(new_queue)
 
     def __str__(self):
-        return f"name:{self.name}, ID:{self.id}"
+        return f"name:{self.name}, ID:{self.id_User}"
 
 
 class Queue:
     def __init__(self, admin=None):
-        self.queue = []
+        self.queue = list()
         self.admin = admin
 
     def get_admin(self):
@@ -29,28 +29,49 @@ class Queue:
         self.queue.append(new_user)
 
     def next_in_queue(self):
-        return self.queue.pop(0), self.queue[0]
-
+        if len(self.queue)>=2:
+            return self.queue.pop(0), self.queue[0]
+        elif len(self.queue) == 1:
+            return self.queue.pop(0), None
+        else: return None, None
     def __iter__(self):
         yield from self.queue
 
-    def search_by_id(self, id):
-        for i in range(len(self.queue)):
-            if self.queue[i].id == id:
-                return i
-        else:
-            return None
 
     def lenght(self):
         return len(self.queue)
 
 
-class Admin(User, Queue):
-    def __init__(self, id, name="Basa_name", name_queue="Base_name", queue=None):
-        print(f"admin init: {name}")
-        User.__init__(id, name)
-        self.queue = Queue(self)
+    def search_by_id(self, id_User):
+        for i in range(self.lenght()):
+            if self.queue[i].id_User == id_User:
+                return i
+        else:
+            return None
 
+
+class Admin(User):
+    def __init__(self, id_Admin, name="Basa_name", name_queue="Base_name", queue=None):
+        print(f"admin init: {name}")
+        User.__init__(self,id_Admin, name)
+        self.admin_queue = Queue(self)
+        self.id_Admin = id_Admin
+        self.name = name
+
+    def add_user(self, new_user: User):
+        self.admin_queue.add_user(new_user)
+
+    def next_in_queue(self):
+        return self.admin_queue.next_in_queue()
+
+    def __iter__(self):
+        yield from self.admin_queue
+
+    def lenght(self):
+        return len(self.admin_queue)
+
+    def search_by_id(self, id_User):
+        return self.admin_queue.search_by_id(id_User)
 
 if __name__ == "__main__":
     a = User(1, 1)
@@ -66,3 +87,4 @@ if __name__ == "__main__":
     for i in q:
         print(i.name)
     print(q.next_in_queue()[1])
+    print(q.lenght())
